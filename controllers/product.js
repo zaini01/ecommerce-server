2156690f5783cf2842ccb38216a28b03dc9db973
zaini01 <o.z41n1@gmail.com>
2021-01-24 11:06:31 +0700

    fix orderby

--- controllers/product.js
+++ controllers/product.js
@@ -55,7 +55,7 @@ class ProductCon {
     }
 
     static findAll(req,res,next){
-        Product.findAll()
+        Product.findAll({order: [[ 'createdAt', 'DESC' ]]})
         .then(data=>{
             res.status(200).json({data})
         })
